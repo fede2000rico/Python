211227c93a87691fb9e07b174d63cc3bfe19e155
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-26 20:24:03 +0100

    Functions\conversion.py

---
+++ Functions/conversion.py
@@ -0,0 +1,13 @@
+#Esercizi con le funzioni di conversione
+
+#Binario
+print("Binario:")
+print(bin(15))
+
+#Esadecimale
+print("\nEsadecimale:")
+print(hex(15))
+
+#Ottale
+print("\nOttale:")
+print(oct(15))
