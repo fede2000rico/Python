7ef141962542d2c485ace6a37adc7d798e920435
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-27 20:18:40 +0100

    Functions\My_functions\01_declaration.py

--- Functions/My_functions/01_declaration.py
+++ Functions/My_functions/01_declaration.py
@@ -1,6 +1,8 @@
-#Esercizi con le prime dichiarazioni di funzioni
+#Esercizi con la dichiarazione di funzioni
 
+#Definizione della funzione quadrato
 def square(x):
     return x*x
 
-print(square(4))
+#Stampa il risultato
+print(square(4)) #16
