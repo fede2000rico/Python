2d27fdc369307da7d553e3e2e18b0f09910204af
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-27 11:23:07 +0100

    Functions\library_sys.py

---
+++ Functions/library_os.py
@@ -0,0 +1,7 @@
+#Esercizi con la libreria os
+import os
+
+#Informazioni generali
+print(os.environ['USERNAME']) #fede2
+
+print(os.environ['SHELL']) #fede2
---
+++ Functions/library_sys.py
@@ -0,0 +1,8 @@
+#Esercizi con la libreria sys
+import sys
+
+#OS
+print(sys.platform) #win32
+
+#Python version
+print(sys.version) #3.7.6
