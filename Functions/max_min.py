3467f3a3a2df6272590666bea9875fbb186d5732
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-27 08:40:56 +0100

    Functions\max_min.py

---
+++ Functions/max_min.py
@@ -0,0 +1,11 @@
+#Funzioni di massimo e minimo nelle sequenze
+mylist=[0,2,1,3,5,4]
+mylist.sort()
+
+#Massimo
+print("Massimo:")
+print(max(mylist))
+
+#Minimo
+print("\nMinimo:")
+print(min(mylist))
