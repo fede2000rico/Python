5c37095f31152e06ecf61646600b87c6e27a3007
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-28 23:46:15 +0100

    Exercises\10_List_Overlap_Comprehensions.py

--- Exercises/10_List_Overlap_Comprehensions.py
+++ Exercises/10_List_Overlap_Comprehensions.py
@@ -20,9 +20,18 @@ Extra:
 1. Randomly generate two lists to test this
 '''
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 1, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-c=[elem for elem in a if(elem in b and elem not in c)]
+#Ciclo su ogni elemento e se duplicato allora lo elimina fino a lasciarne uno solo
+for k in a:
+	if(a.count(k)>1):
+		while(a.count(k)!=1):
+			a.remove(k)
+
+a.sort() #Ordina la lista
+print(a)
+
+c=[elem for elem in a if(elem in b)] #List comprehension
 
 print(c)
