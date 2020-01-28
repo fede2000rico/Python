06a355c4c122a419f4ca361ed95833af52d58742
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-28 18:27:09 +0100

    Exercises\09_Guessing_Game_One.py

---
+++ Exercises/09_Guessing_Game_One.py
@@ -0,0 +1,23 @@
+'''
+Generate a random number between 1 and 9 (including 1 and 9).
+Ask the user to guess the number, then tell them whether they guessed too low,
+too high, or exactly right. (Hint: remember to use the user input lessons from the very first exercise)
+
+Extras:
+1. Keep the game going until the user types “exit”
+2. Keep track of how many guesses the user has taken, and when the game ends, print this out.
+'''
+
+import random
+
+num=random.randrange(1,10) #Libreria random che genera numeri random in un intervallo
+
+print("Indovinare il numero:")
+user_num=int(input())
+
+if(num==user_num):
+    print("Esatto")
+elif(num<user_num):
+    print("Troppo alto")
+else:
+    print("Troppo basso")
---
+++ Functions/Built-in/library_random.py
@@ -0,0 +1,4 @@
+#Esercizi con la libreria random
+import random
+
+print(random.randrange(1,10)) #[1,10) 10 escluso
