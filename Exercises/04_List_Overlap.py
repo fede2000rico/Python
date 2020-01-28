ef60e0ca3bf5454b33bd788f68c7943a65d7e460
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-28 15:38:27 +0100

    Exercises\04_List_Overlap.py

---
+++ Exercises/04_List_Overlap.py
@@ -0,0 +1,25 @@
+'''
+Take two lists, say for example these two:
+
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists
+(without duplicates). Make sure your program works on two lists of different sizes.
+
+Extras:
+
+Randomly generate two lists to test this
+Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+'''
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+intersection=[]
+
+#Ciclo su ogni elemento di un insieme e verifico che sia presente nell'altro, se così lo aggiunge all'intersezione
+for i in a:
+    if(i in b):
+        intersection.append(i)
+
+print(intersection)
