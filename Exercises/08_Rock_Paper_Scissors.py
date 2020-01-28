2a13eb7cd8f8373e29c560c481e32c0de063bdee
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-28 16:09:04 +0100

    Exercises\08_Rock_Paper_Scissors.py

---
+++ Exercises/08_Rock_Paper_Scissors.py
@@ -0,0 +1,21 @@
+'''
+Make a two-player Rock-Paper-Scissors game.
+(Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner,
+and ask if the players want to start a new game)
+
+Remember the rules:
+    Rock beats scissors
+    Scissors beats paper
+    Paper beats rock
+'''
+
+newgame=1 #Variabile per continuare il gioco
+
+dictionary={R:1,P:2,S:3} #Dizionario per convertire le scelte
+
+while(newgame):
+    print("Player1 choose R-P-S:") #Scelta giocatore 1
+    choice1=input() #Lettura tastiera
+
+    print("Player2 choose R-P-S:") #Scelta giocatore 2
+    choice2=input() #Lettura tastiera
