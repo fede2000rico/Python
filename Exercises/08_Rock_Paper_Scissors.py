c2e5d6c86f108d6bb2b9abff49714747d0e04f26
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-28 18:12:06 +0100

    Exercises\08_Rock_Paper_Scissors.py

--- Exercises/08_Rock_Paper_Scissors.py
+++ Exercises/08_Rock_Paper_Scissors.py
@@ -11,7 +11,7 @@ Remember the rules:
 
 newgame=1 #Variabile per continuare il gioco
 
-dictionary={R:1,S:2,P:3} #Dizionario per convertire le scelte
+dictionary={'R':1,'S':2,'P':3} #Dizionario per convertire le scelte
 
 while(newgame):
     print("Player1 choose R-P-S:") #Scelta giocatore 1
@@ -20,4 +20,32 @@ while(newgame):
     print("Player2 choose R-P-S:") #Scelta giocatore 2
     choice2=input() #Lettura tastiera
 
-#ESEMPIO
+    #CONFRONTO DI OGNI POSSIBILE GIOCATA
+    if(dictionary[choice1]==1):
+        if(dictionary[choice2]==1):
+            print("Draft")
+        if(dictionary[choice2]==2):
+            print("Player1 win")
+        if(dictionary[choice2]==3):
+            print("Player2 win")
+
+    if(dictionary[choice1]==2):
+        if(dictionary[choice2]==1):
+            print("Player1 win")
+        if(dictionary[choice2]==2):
+            print("Draft")
+        if(dictionary[choice2]==3):
+            print("Player2 win")
+
+    if(dictionary[choice1]==3):
+        if(dictionary[choice2]==1):
+            print("Player1 win")
+        if(dictionary[choice2]==2):
+            print("Player2 win")
+        if(dictionary[choice2]==3):
+            print("Draft")
+
+    #OPERAZIONI CHE INTERROMPONO IL CICLO IN CASO DI RISPOSTA NEGATIVA
+    print("Want to play again? Y or N")
+    if(input()=='N'):
+        newgame=0
