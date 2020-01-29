66b91f08dbea7fb93b60e0618509feacd8be8e4d
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-29 18:55:00 +0100

    Exercises\19_Decode_A_Web_Page_Two.py

--- Exercises/19_Decode_A_Web_Page_Two.py
+++ Exercises/19_Decode_A_Web_Page_Two.py
@@ -11,17 +11,28 @@ of the exercise posted here.)
 This will just print the full text of the article to the screen. It will not make it easy to read, so next exercise
 we will learn how to write this text to a .txt file.
 '''
+#IMPORTO LIBRERIE NECESSARIE
 import requests
 from bs4 import BeautifulSoup
 
+#SET DEL SITO WEB CHE VOGLIO ANALIZZARE
 base_url = "https://www.practicepython.org/"
+
+#"CHIEDO" UNA COPIA DELL'HTML
 r = requests.get(base_url)
 
-
+#Creo una soup
 soup = BeautifulSoup(r.text, "html.parser")
 
+#Seleziono il blocco dell'html
 text = soup.select("a")
+
+#Apro il file di scrittura
+file=open('C:/Users/fede2/OneDrive/Documenti/GitHub/Python/Exercises/data_file/19_data.txt','w')
 
 #Stampa ogni elemento all'interno di text
 for elem in text:
-  print(elem.text)
+    file.write(elem.text +'\n')
+    print(elem.text)
+
+file.close()
