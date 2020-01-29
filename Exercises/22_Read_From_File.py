d5be3748e285177ae9d7912ba9ba4dbef999d9ba
federicopiol <fede2000rico@users.noreply.github.com>
2020-01-29 19:18:45 +0100

    Exercises\22_Read_From_File.py

---
+++ Exercises/22_Read_From_File.py
@@ -0,0 +1,11 @@
+'''
+Given a .txt file that has a list of a bunch of names, count how many of each name there are in the file,
+and print out the results to the screen. I have a .txt file for you, if you want to use it!
+'''
+
+#Leggo il file dell'esercizio 19
+file=open('C:/Users/fede2/OneDrive/Documenti/GitHub/Python/Exercises/data_file/19_data.txt','r')
+
+#Per ogni riga stampa a video
+for line in file:
+    print(line)
